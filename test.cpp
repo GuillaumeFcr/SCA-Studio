99e8835b5ab662c18bfa06aed0ec75b93d820277
GuillaumeFcr <flogui.foucher@gmail.com>
2026-03-10 17:38:18 +0100

    contd

--- test.cpp
+++ test.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include "Install 1.9.20/bps202.h"
-using namespace std;
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "bps202.h"
 
 int main() {
-    DLL::bps_init();
-    char*e=DLL::bps_get_probename();
-    cout<< *e << endl;
+	double* b = new double;
+    int a=bps_init();
+	while (bps_get_status()==-1){}
+	std::cout << "connecte"<<std::endl;
+	std::cout << bps_get_pulse_level_count()<<std::endl;
+    int e=bps_get_pulse_levels(b,46);
+	std::cout << bps_get_pulse_range_count()<<std::endl;
+	std::cout<<e;
+	std::cout << std::endl;
+	for (int i=0;i<46;i++){
+		std::cout<< *(b+i)<<" ";
+	}
+	std::cout<<std::endl;
     return 0;
 }
+
+#ifdef __cplusplus
+}
+#endif
